ac4c4fa74996a9c50d99f7deb68ed083def302e2
josejustotena0@gmail.com <josejustotena0@gmail.com>
2022-04-24 17:32:34 +0200

    implemented signup and login test

--- app/src/test/java/com/example/prueba2/smokeTest/LoginTest.java
+++ app/src/test/java/com/example/prueba2/smokeTest/LoginTest.java
@@ -1,5 +1,32 @@
 package com.example.prueba2.smokeTest;
 
+import com.example.prueba2.Sistema;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class LoginTest {
+
+    private final Sistema sistema;
+
+    public LoginTest(Sistema sistema) {
+        this.sistema = sistema;
+    }
+
+    @Test
+    public void loginRegisteredUser(){
+        String name = "RegisteredName";
+        String password = "RegisteredPassword";
+        sistema.register(name, password);
+        assertTrue(sistema.logIn(name, password));
+    }
+
+    @Test
+    public void loginNonRegisteredUser(){
+        String name = "NonRegisteredName";
+        String password = "NonRegisteredPassword";
+        assertFalse(sistema.logIn(name, password));
+    }
 
 }
--- app/src/test/java/com/example/prueba2/smokeTest/SignupTest.java
+++ app/src/test/java/com/example/prueba2/smokeTest/SignupTest.java
@@ -1,5 +1,32 @@
 package com.example.prueba2.smokeTest;
 
+import com.example.prueba2.Sistema;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class SignupTest {
+
+    private final Sistema sistema;
+
+    public SignupTest(Sistema sistema) {
+        this.sistema = sistema;
+    }
+
+    @Test
+    public void registerNewUser(){
+        String name = "Name1";
+        String password = "Password1";
+        assertTrue(sistema.register(name, password));
+    }
+
+    @Test
+    public void registerExistingUser(){
+        String name = "Name2";
+        String password = "Password2";
+        sistema.register(name, password);
+        assertFalse(sistema.register(name, password));
+    }
 
 }
